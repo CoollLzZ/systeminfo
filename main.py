de361595a2a714fa9711d83d000e9bfdcebb121f
CoollLzZ <--list>
2022-09-07 16:50:43 +0530

    Adding Manually

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ def info_gen(day, sr):
 # Todo: Creating function for pushing data-file into Public repo
 def git_push(date, sr):
     """This function needs some args: date=today's date and sr=serial number"""
-    #command = "git config --global --add safe.directory 'systeminfo'"
-    #subprocess.run(command, shell=True)
     command = "git add ."
     subprocess.run(command, shell=True)
     command = f'git commit -m "Pushing info on {date} for serial number:{sr}"'
     subprocess.run(command, shell=True)
     command = "git add ."
+    subprocess.run(command, shell=True)
+    command = f"git remote add origin '{GIT_URL}'"
     subprocess.run(command, shell=True)
 
 
