99f88eb0924677cf352bc3a250ea1ab6c014cc6b
CoollLzZ <mohitkamble.mk@gmail.com>
2022-09-07 15:38:04 +0530

    Adding the files for testing

---
+++ ReadME.md
@@ -0,0 +1,14 @@
+## Description
+
+Collecting users M/C information such like **Hardware & OS Information, Installed software, version, date, etc** and saving in the unique directory of the particular serial number in the file with today's date and serial number associated with it.
+
+>-  Alteration in the code leads to script fails during execution.
+>- Sensitive information like MAC, IP's using for Minutus Purpose.
+>- Before proceeding to run this script please go through the Prerequisites section.
+
+## Prerequisites
+It is very important to fulfill the necessary dependencies to be satisfied before execution. 
+>-**Git cmd**
+> 
+>-**Python Interpreter 3.6 or higher**
+
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 import datetime
 import os
+import subprocess
+
+
+# Todo: Creating function for generating data file
+def info_gen(day, sr):
+    """Please Provide day=today's date and sr=serial number of the M/C"""
+    with open(mode='w', file=f'Data/{sr}/{sr}_{day}') as info:
+        info.write(f'\nThis file is registered with Serial Number: {sr}\n\n')
+        info.write("---------------------------------------------------------------------------------------------\n")
+        info.write("--------------------------------------Detailed System Info-----------------------------------\n")
+        info.write("---------------------------------------------------------------------------------------------\n")
+        info.write(systemInfo)
+        info.write("\n\n\n")
+        info.write("---------------------------------------------------------------------------------------------\n")
+        info.write("--------------------------------------Installed Software's-----------------------------------\n")
+        info.write("---------------------------------------------------------------------------------------------\n\n")
+        info.write(Installed_Software)
+
 
 # Todo: Getting Current date
 today = datetime.date.today()
@@ -8,28 +26,27 @@ today = datetime.date.today()
 cmd = 'wmic bios get serialnumber'
 SR_number = os.popen(cmd).read()
 SR_number = SR_number.split("\n")[2]
+SR_number = SR_number[0:7]
 
 # Todo: Collecting the system's information
 cmd = 'systeminfo'
 systemInfo = os.popen(cmd).read()
 
 # Todo: Collecting Installed Software list
-cmd = "wmic product get Name, Version"
+cmd = "wmic product get Name, Version, InstallDate"
 Installed_Software = os.popen(cmd).read()
 
-with open(mode='w', file=f'{today}_{SR_number[0:7]}.txt') as info:
-    info.write(f'\nThis file is registered with Serial Number: {SR_number}\n\n')
-    info.write("---------------------------------------------------------------------------------------------\n")
-    info.write("--------------------------------------Detailed System Info-----------------------------------\n")
-    info.write("---------------------------------------------------------------------------------------------\n")
-    info.write(systemInfo)
-    info.write("\n\n\n")
-    info.write("---------------------------------------------------------------------------------------------\n")
-    info.write("--------------------------------------Installed Software's-----------------------------------\n")
-    info.write("---------------------------------------------------------------------------------------------\n\n")
-    info.write(Installed_Software)
+# Todo: Directory confirmation of this serial number, Otherwise create a separate directory for this serial.
+cmd = f"dir Data| findstr {SR_number}"
+folder = subprocess.run(cmd, shell=True)
 
-# Todo: Pushing info file to public repository
-GIT_URL= 'https://github.com/CoollLzZ/systeminfo.git'
-cmd = 'git add .'
-os.popen(cmd)
+# Todo: Checking the return-code of last command.
+if folder.returncode == 0:
+    # That means the folder for this serial number exists
+    info_gen(day=today, sr=SR_number)
+else:
+    # That means we have to create a folder for new serial number
+    # Todo: Creating sr number folder for the new M/C
+    cmd = f"mkdir Data\{SR_number}"
+    folder = subprocess.run(cmd, shell=True)
+    info_gen(day=today, sr=SR_number)
